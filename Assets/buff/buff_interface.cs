12a865a4c5e5850cafed32ee6e14c65ca40a899f
Eycia Zhou <zhou.eycia@gmail.com>
2016-03-20 15:00:39 +0800

    attributes added

---
+++ Assets/buff/buff_interface.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface buff_interface {
+	void calculate(attributes.mid_attributes mid);
+	attributes.t_buff_change_callback change_callback { get; set; }
+	int priority { get; set; }
+	bool vaild { get; }
+	attributes attr { get; set; }
+	System.Guid guid { get; set; }
+}
---
+++ Assets/view/controller_interface.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public interface controller_interface
+{
+	attributes attr { get; set; }
+	void update_controller ();
+	System.Guid guid { get; set; }
+}
